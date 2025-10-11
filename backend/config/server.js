3eda74c2c447f6d6043802384f2a71c1697f9538
DeepakBaligar <deepakbaligar-83@users.noreply.github.com>
2025-10-12 00:53:45 +0530

    nadi nadi 7

--- backend/config/server.js
+++ backend/config/server.js
@@ -8,11 +8,10 @@
 //     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
 //   },
 // });
-
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("PUBLIC_URL", "https://typescript-blog-jet.vercel.app"), // ✅ Add this line
+  url: env("PUBLIC_URL", "https://strapi-backend-connect.onrender.com"), // ✅ Your Strapi public URL
   app: {
     keys: env.array("APP_KEYS", []),
   },
@@ -20,3 +19,4 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
+
